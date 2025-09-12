f6ebf89c674379a7e2bb076e57bb300f3222d2b1
Erik Mota <seriikmota.dev@gmail.com>
2025-09-12 20:45:13 -0300

    adicionando verificação de cabeçalho para redirecionamento de links curtos

--- src/app/route/link.route.ts
+++ src/app/route/link.route.ts
@@ -5,9 +5,10 @@ export const linkRouter = Router();
 
 linkRouter.get("/:id", async (req, res) => {
     console.log('ShortLink Log Headers: ', req.headers);
-    const id = req.params.id;
-    const longUrl = await resolveShortLink(id);
-    if (!longUrl) return res.status(404).send("Link expirado ou inválido");
-
-    res.redirect(longUrl);
+    if (req.header('accept-language')) {
+        const id = req.params.id;
+        const longUrl = await resolveShortLink(id);
+        if (!longUrl) return res.status(404).send("Link expirado ou inválido");
+        res.redirect(longUrl);
+    }
 });
