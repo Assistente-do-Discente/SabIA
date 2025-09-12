12622dc788a064ba091282eafb0e935c9179b236
Erik Mota <seriikmota.dev@gmail.com>
2025-09-12 20:37:17 -0300

    adicionando log de cabeçalhos para links curtos

--- src/app/route/link.route.ts
+++ src/app/route/link.route.ts
@@ -4,6 +4,7 @@ import { resolveShortLink } from "../service/shortlink.service";
 export const linkRouter = Router();
 
 linkRouter.get("/:id", async (req, res) => {
+    console.log('ShortLink Log Headers: ', req.headers);
     const id = req.params.id;
     const longUrl = await resolveShortLink(id);
     if (!longUrl) return res.status(404).send("Link expirado ou inválido");
