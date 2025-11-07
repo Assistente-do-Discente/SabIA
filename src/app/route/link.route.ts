73205bdaa2a4e137a00365dcf372d704e9f43c45
Erik Mota <seriikmota.dev@gmail.com>
2025-11-07 18:48:58 -0300

    debug header

--- src/app/route/link.route.ts
+++ src/app/route/link.route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import {deleteShortLink, resolveShortLink} from "../service/shortlink.service";
+import logger from "../config/logger.config";
 
 export const linkRouter = Router();
 
 linkRouter.get("/:id", async (req, res) => {
+    logger.info(req.headers, 'headers: ');
+
     const id = req.params.id;
     const longUrl = await resolveShortLink(id);
     if (!longUrl) return res.status(404).send("Link expirado ou inválido");
